151f41b61bbad7a45cd8beab6edb6655758de59c
somakihiro <aki.air@icloud.com>
2020-01-17 06:13:48 +0900

    Add firebase sdk

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCG2x9RNJGDgGf4RMKIHMqv5BpXnxuc_2s",
+  authDomain: "cocopure-nagi.firebaseapp.com",
+  databaseURL: "https://cocopure-nagi.firebaseio.com",
+  projectId: "cocopure-nagi",
+  storageBucket: "cocopure-nagi.appspot.com",
+  messagingSenderId: "38957158257",
+  appId: "1:38957158257:web:56933c5b05d7765310b0d0",
+  measurementId: "G-PZ864SKXM8",
+}
+
+firebase.initializeApp(firebaseConfig)
+
+export const db = firebase.firestore()
+export default firebase
